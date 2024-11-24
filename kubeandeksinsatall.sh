320d7270980f97edcf636ade67a1c14ca6494e20
venugopal <mallavallivenu@gmail.com>
2024-11-24 14:31:40 +0530

    install kubeks

---
+++ kubeandeksinsatall.sh
@@ -0,0 +1,26 @@
+echo "install kubectl"
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-09-12/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv kubectl /usr/local/bin/kubectl
+kubectl version
+
+
+eks "install eksctl"
+
+# for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
+ARCH=
+
+PLATFORM=$(uname -s)_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+
+# (Optional) Verify checksum
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+
+sudo mv /tmp/eksctl /usr/local/bin
+
+echo "give AWS Credetial"
+
+aws configure
